4d9305c141cb1273374843912081402d567916b8
선비 <sunbi2003@gmail.com>
2026-03-22 00:04:53 +0900

    ch02 과제

--- src/hw/ch02/Main.java
+++ src/hw/ch02/Main.java
@@ -3,6 +3,8 @@ package hw.ch02;
 //Step3: Main 클래스 생성
 public class Main {
     public static void main(String[] args) {
+        System.out.println("20231009 선비");
+
         Charger charger = new KoreanOutletAdapter();
         Smartphone smartphone = new Smartphone(charger);
         smartphone.charge();
